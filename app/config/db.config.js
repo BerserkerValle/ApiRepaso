4d49756ae598fe46f6eb3250fca512c8d21437c3
BerserkerValle <fsamayoav@miumg.edu.gt>
2024-09-16 23:41:15 -0800

    autor 1.3

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -29,6 +29,6 @@ db.sequelize = sequelize;
 db.Customer = require('../models/customer.model.js')(sequelize, Sequelize);
 db.Usuario = require('../models/usuario.js')(sequelize, Sequelize);
 db.Libro = require('../models/libro.js')(sequelize, Sequelize);
-db.Autor = require('../models/autor.js')(sequelize, Sequelizer);
+db.Autor = require('../models/autor.js')(sequelize, Sequelize);
 
 module.exports = db;
